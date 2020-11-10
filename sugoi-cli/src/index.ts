13d2a26e9e235fce3c447ffe7df477d551852d9e
streetcatsky <streetcatsky@gmail.com>
2020-11-10 18:11:50 +0800

    - testing

---
+++ sugoi-cli/src/index.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+'use strict';
+
+const currentNodeVersion = process.versions.node;
+const semver = currentNodeVersion.split('.');
+const major = semver[0];
+
+if (+major < 10) {
+  console.error(
+    'You are running Node ' +
+      currentNodeVersion +
+      '.\n' +
+      'Use sugoi cli requires Node 10 or higher. \n' +
+      'Please update your version of Node.'
+  );
+  process.exit(1);
+}
+
+ import('./createSugoiTemplate').then(res=>{
+  res.init()
+}) 
