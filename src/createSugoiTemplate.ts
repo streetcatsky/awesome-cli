0fed7ac3aad4456b16960dd6b7f9822a332ff761
streetcatsky <43740219+streetcatsky@users.noreply.github.com>
2021-01-23 17:29:12 +0800

    npmignore rename gitignore

--- src/createSugoiTemplate.ts
+++ src/createSugoiTemplate.ts
@@ -146,7 +146,7 @@ export default class CreateSugoiTemplate {
     const spinner = ora(`Creating project contents.`)
     spinner.start()
     shell.cp('-R', this.templatePath + '/*', this.projectPath)
-    shell.cp('-R', this.templatePath + '/.*', this.projectPath)
+    shell.cp('-R', this.templatePath + '/.npmignore', this.projectPath + '/.gitignore')
     spinner.succeed()
   }
 
